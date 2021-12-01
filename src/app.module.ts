ca61083c086a09e7370e204fefcc20502581860d
Oscar-pp <oscar22junio@gmail.com>
2021-12-01 20:36:40 +0100

    first

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { SupplierModule } from './supplier/supplier.module';
+import { UserModule } from './user/user.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule, SupplierModule, UserModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
